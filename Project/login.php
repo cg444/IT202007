c6d0a179e24efcdee2191e79498998b527e08483
cg444 <cg444@njit.edu>
2020-12-09 11:01:09 -0500

    further testing

--- Project/login.php
+++ Project/login.php
@@ -2,8 +2,6 @@
     <form method="POST">
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" required/>
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required/>
         <label for="p1">Password:</label>
         <input type="password" id="p1" name="password" required/>
         <input type="submit" name="login" value="Login"/>
@@ -32,7 +30,7 @@ if (isset($_POST["login"])) {
     if ($isValid) {
         $db = getDB();
         if (isset($db)) {
-            $stmt = $db->prepare("SELECT id, email, username, password from Users WHERE email = :email or username = :email 1");
+            $stmt = $db->prepare("SELECT id, email, username, password from Users WHERE email = :email OR username = :email LIMIT 1");
 
             $params = array(":email" => $email);
             $r = $stmt->execute($params);
