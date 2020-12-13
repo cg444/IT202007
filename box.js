9b1a95bd999d74faba8a7128db5d647783d2f69d
cg444 <cg444@njit.edu>
2020-12-12 20:00:38 -0500

    changed red to green backgrnd

--- box.js
+++ box.js
@@ -109,7 +109,7 @@ var myVar = setInterval(setColor, 1000);
 
 function setColor() {
     var x = document.body;
-    x.style.backgroundColor = x.style.backgroundColor == "blue" ? "red" : "blue";
+    x.style.backgroundColor = x.style.backgroundColor == "blue" ? "green" : "blue";
 }
 
 function stopColor() {
