2efc8bf7075d8c3daa32341d218077b8cb73188d
cg444 <cg444@njit.edu>
2020-12-14 12:35:16 -0500

    testing cart stuff

--- Project/lib/helpers.php
+++ Project/lib/helpers.php
@@ -68,5 +68,11 @@ function getMessages() {
     return array();
 }
 
+function getURL($path) {
+    if (substr($path,0,1) == "/") {
+        return $path;
+    }
+    return $_SERVER["CONTEXT_PREFIX"]. "/IT202-007/Project/$path";
+}
 //end flash
 ?>
