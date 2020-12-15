8b45671e5bb514ef9763252114273d8ed47199b8
cg444 <cg444@njit.edu>
2020-12-15 14:22:05 -0500

    Creating customer cart list

--- Project/customer_list_products.php
+++ Project/customer_list_products.php
@@ -2,43 +2,13 @@
 <?php
 $query = "";
 $results = [];
-$selectedCat = '';
-$dbQuery = "SELECT name, id, price, category, quantity, description, visibility, user_id from Products WHERE quantity >0 AND visibility = 1";
-$sort = "default";
-$params = [];
 if (isset($_POST["query"])) {
     $query = $_POST["query"];
 }
-
-$db = getDB();
-$stmt = $db->prepare("SELECT distinct category from Products;");
-$r = $stmt->execute();
-if ($r) {
-    $cats = $stmt->fetchAll(PDO::FETCH_ASSOC);
-}
-else {
-    flash("There was a problem fetching the results");
-}
-
-if (isset($_POST["Search"])) {
-
-    $selectedCat = $_POST['category'];
-    if ($selectedCat != -1){
-        $dbQuery .= " AND category = :cat ";
-        $params[":cat"] = $selectedCat;
-    }
-    if ($query != "") {
-        $dbQuery .= " AND name like :q ";
-        $params[":q"] = $query;
-    }
-    if(isset($_POST["sort"])) {
-        $sort = "price";
-        $dbQuery .= " ORDER BY $sort ASC";
-    }
-
+if (isset($_POST["search"]) && !empty($query)) {
     $db = getDB();
-    $stmt = $db->prepare($dbQuery);
-    $r = $stmt->execute($params);
+    $stmt = $db->prepare("SELECT id, name, price, quantity, description, user_id from Products WHERE name like :q LIMIT 10");
+    $r = $stmt->execute([":q" => "%$query%"]);
     if ($r) {
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
@@ -46,33 +16,19 @@ if (isset($_POST["Search"])) {
         flash("There was a problem fetching the results");
     }
 }
-
-
 ?>
-<h3>Search</h3>
-
+<h3>List Product</h3>
 <form method="POST">
-    <div class="form-group">
-    <select name="category" value="<?php echo $result["category"];?>" >
-            <option value="-1">None</option>
-            <?php foreach ($cats as $cat): ?>
-                <option value="<?php safer_echo($cat["category"]); ?>"
-                ><?php safer_echo($cat["category"]); ?></option>
-            <?php endforeach; ?>
-        </select>
-        <input name="query" placeholder="Search" value="<?php safer_echo($query); ?>"/>
-        <input type="submit" value="search" name="Search"/>
-        <label>Sort by Ascending Price</label>
-        <input type="radio" value ="sort" name = "sort"/>
-    </div>
+    <input name="query" placeholder="Search" value="<?php safer_echo($query); ?>"/>
+    <input type="submit" value="Search" name="search"/>
 </form>
-
 <div class="results">
     <?php if (count($results) > 0): ?>
         <div class="list-group">
-        <?php foreach ($results as $r): ?>
-                    <div class="list-group-item">
+            <?php foreach ($results as $r): ?>
+                <div class="list-group-item">
                     <div>
+                        <div>Name:</div>
                         <div><?php safer_echo($r["name"]); ?></div>
                     </div>
                     <div>
@@ -80,20 +36,19 @@ if (isset($_POST["Search"])) {
                         <div><?php safer_echo($r["price"]); ?></div>
                     </div>
                     <div>
-                        <?php if ($r["quantity"] < 10): ?>
-
-                        <div><?php safer_echo("Only " . $r["quantity"] . " left in stock, order soon."); ?></div>
-                   <?php endif;?>
+                        <div>Quantity:</div>
+                        <div><?php safer_echo($r["quantity"]); ?></div>
                     </div>
                     <div>
                         <div>Description:</div>
                         <div><?php safer_echo($r["description"]); ?></div>
                     </div>
                     <div>
-                        <a type="button" href="user_view_products.php?id=<?php safer_echo($r['id']); ?>">View Product</a>
+                        <a type="button" href="test_edit_products.php?id=<?php safer_echo($r['id']); ?>">Edit</a>
+                        <a type="button" href="test_view_products.php?id=<?php safer_echo($r['id']); ?>">View</a>
                     </div>
                 </div>
-        <?php endforeach; ?>
+            <?php endforeach; ?>
         </div>
     <?php else: ?>
         <p>No results</p>
