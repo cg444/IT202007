95ed9387861de8023b658c7b2d74abda47598950
cg444 <cg444@njit.edu>
2020-12-15 15:29:55 -0500

    editing nav

--- Project/partials/nav.php
+++ Project/partials/nav.php
@@ -10,7 +10,6 @@ require_once(__DIR__ . "/../lib/helpers.php");
             <li><a href="login.php">Login</a></li>
             <li><a href="register.php">Register</a></li>
             <li><a href="customer_list_products.php">View Products</a></li>
-            <li><a href="customer_list_cart.php">View Cart</a></li>
         <?php endif; ?>
         <?php if (has_role("Admin")): ?>
             <li><a href="test_create_products.php">Create Products</a></li>
@@ -21,6 +20,7 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <?php if (is_logged_in()): ?>
             <li><a href="profile.php">Profile</a></li>
             <li><a href="logout.php">Logout</a></li>
+            <li><a href="customer_list_cart.php">View Cart</a></li>
         <?php endif; ?>
     </ul>
 </nav>
