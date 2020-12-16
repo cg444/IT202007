ba256a0a1727bb82d5b6992e9b3ab1015040fb8a
cg444 <cg444@njit.edu>
2020-12-15 22:48:48 -0500

    trying to add subtotal again

--- Project/customer_list_cart.php
+++ Project/customer_list_cart.php
@@ -14,7 +14,7 @@ if (isset($_POST["query"])) {
 }
 if (isset($_POST["search"]) && !empty($query)) {
     $db = getDB();
-    $stmt = $db->prepare("SELECT product_id, name, Cart.id, Cart.quantity, price From Cart JOIN Products on Cart.product_id = Products.id where Cart.user_id=:user_id and Products.name like :q LIMIT 10");
+    $stmt = $db->prepare("SELECT product_id, name, Cart.id, Cart.quantity From Cart JOIN Products on Cart.product_id = Products.id where Cart.user_id=:user_id and Products.name like :q LIMIT 10");
     $r = $stmt->execute([
         ":q" => "%$query%",
         ":user_id"=> $id,
