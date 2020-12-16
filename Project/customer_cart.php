a81c523ada506e68d88fa10a117e3ddc6d9cb75c
cg444 <cg444@njit.edu>
2020-12-15 20:43:28 -0500

    all carts are fixed

--- Project/customer_cart.php
+++ Project/customer_cart.php
@@ -39,7 +39,7 @@ if (isset($_POST["save"])) {
     $user = get_user_id();
     $db = getDB();
     $stmt = $db->prepare("SELECT FROM Products(price) VALUES(:pr)");
-    $stmt = $db->prepare("INSERT INTO Cart (product_id, price, quantity,user_id) VALUES(:id, :pr, :quantity, :user) on duplicate key update quantity = :quantity");
+    $stmt = $db->prepare("INSERT INTO Cart (product_id, price, quantity,user_id) VALUES(:id, :pr, :quantity, :user) on duplicate key update quantity = quantity + :quantity");
     $r = $stmt->execute([
         ":id"=>$id,
         ":pr"=>$pr,
