311776d706b14d2c214ea05aafefaf08adb551e0
cg444 <cg444@njit.edu>
2020-12-15 21:14:21 -0500

    trying to get the cart to print more

--- Project/customer_list_cart.php
+++ Project/customer_list_cart.php
@@ -57,7 +57,7 @@ if (isset($_POST["search"]) && !empty($query)) {
                 <?php endforeach; ?>
             </div>
         <?php else: ?>
-            <p>No results</p>
+            <?php safer_echo($results[])
         <?php endif; ?>
     </div>
 <?php require(__DIR__ . "/partials/flash.php");
