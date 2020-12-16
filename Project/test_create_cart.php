f562dfe3757510ea798f0b54fb1b9a0dd641a2bc
cg444 <cg444@njit.edu>
2020-12-15 19:49:33 -0500

    fixing carts

--- Project/test_create_cart.php
+++ Project/test_create_cart.php
@@ -15,10 +15,10 @@ $result = [];
 ?>
     <h3>Create Cart</h3>
     <form method="POST">
-        <select name="product_id" value="<?php echo $result["id"];?>" >
+        <select name="product_id" value="<?php echo $result["product_id"];?>" >
             <option value="-1">None</option>
             <?php foreach ($products as $product): ?>
-                <option value="<?php safer_echo($product["id"]); ?>"
+                <option value="<?php safer_echo($product["name"]); ?>"
                 ><?php safer_echo($product["name"]); ?></option>
             <?php endforeach; ?>
         </select>
