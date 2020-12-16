23d3889e4f5c32acbfe3d7fc69658728ef6591e5
cg444 <cg444@njit.edu>
2020-12-15 22:11:13 -0500

    allowing products to get deleted from cart

--- Project/test_edit_cart.php
+++ Project/test_edit_cart.php
@@ -49,7 +49,7 @@ $result = [];
 if (isset($CartID)) {
     $CartID = $_GET["id"];
     $db = getDB();
-    $stmt = $db->prepare("SELECT * FROM Cart where id = :id");
+    $stmt = $db->prepare("SELECT * FROM Cart where id = :id DELETE FROM Cart where quantity = 0");
     $r = $stmt->execute([":id" => $CartID]);
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
 }
