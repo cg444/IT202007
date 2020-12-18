57e31488afcc8d0beef2186f4d0d75d127342199
cg444 <cg444@njit.edu>
2020-12-17 20:48:54 -0500

    added back edit products page for admin

--- Project/partials/nav.php
+++ Project/partials/nav.php
@@ -13,6 +13,7 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <?php endif; ?>
         <?php if (has_role("Admin")): ?>
             <li><a href="test_create_products.php">Create Products</a></li>
+            <li><a href="test_edit_products.php">Edit Products</a></li>
         <?php endif; ?>
         <?php if (is_logged_in()): ?>
             <li><a href="profile.php">Profile</a></li>
