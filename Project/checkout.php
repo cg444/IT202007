62abc418e55100c7e446034fe6e0f2473a75aa06
cg444 <cg444@njit.edu>
2020-12-17 20:04:48 -0500

    testing things

--- Project/checkout.php
+++ Project/checkout.php
@@ -10,7 +10,7 @@ if (!is_logged_in()) {
 <?php
 $isValid = true;
 $address = "";
-$PM = "";
+$Payment = "";
 if (isset($_POST["place"])){
 
     if( isset($_POST["address"])){
@@ -30,7 +30,7 @@ if (isset($_POST["place"])){
         $address = $address . ", " .$_POST["zip"];
     }
     if( isset($_POST["payment"])){
-        $PM = $_POST["payment"];
+        $Payment = $_POST["payment"];
     }
 
     if (!isset($_POST["address"])  || !isset($_POST["city"]) || !isset($_POST["state"]) ||
@@ -60,7 +60,6 @@ if (isset($_POST["place"])){
             }
         } else{flash("You do not have a valid ID");}
 
-        //var_export($results);
         // calculating total and checking for quantity
         $quantityCheck = true;
         $total = 0;
@@ -83,7 +82,7 @@ if (isset($_POST["place"])){
             $r = $stmt->execute([":user_id" => $id,
                 ":total_price" => $total,
                 ":address" => $address,
-                ":Payment" => $PM
+                ":Payment" => $Payment
             ]);
             $e = $stmt->errorInfo();
             if ($e[0] == "00000") {
@@ -158,77 +157,25 @@ if (isset($_POST["place"])){
     <form method = "POST">
         <div class="form-group">
             <h1> ADDRESS </h1>
-            <label for="inputAddress">Address</label>
-            <input type="text" name="address" class="form-control" id="inputAddress" placeholder="1234 Main St" required>
+            <label for="Address">Address</label>
+            <input type="text" name="address" class="form-control" id="Address" placeholder="Street Address" required>
         </div>
         <div class="form-group">
-            <label for="inputAddress2">Address 2</label>
-            <input type="text" name="address2" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            <label for="Appt.">Address 2</label>
+            <input type="text" name="address2" class="form-control" id="Appt." placeholder="">
         </div>
-        <div class="form-row">
+        <div class="form-group">
             <div class="form-group col-md-6">
-                <label for="inputCity">City</label>
-                <input type="text" name="city" class="form-control" id="inputCity" required>
+                <label for="City">City</label>
+                <input type="text" name="city" class="form-control" id="City" placeholder="ie. Newark" required>
             </div>
-            <div class="form-group col-md-4">
-                <label for="inputState">State</label>
-                <select  name = "state" id="inputState" class="form-control" required>
-                    <option value="AL">Alabama</option>
-                    <option value="AK">Alaska</option>
-                    <option value="AZ">Arizona</option>
-                    <option value="AR">Arkansas</option>
-                    <option value="CA">California</option>
-                    <option value="CO">Colorado</option>
-                    <option value="CT">Connecticut</option>
-                    <option value="DE">Delaware</option>
-                    <option value="DC">District Of Columbia</option>
-                    <option value="FL">Florida</option>
-                    <option value="GA">Georgia</option>
-                    <option value="HI">Hawaii</option>
-                    <option value="ID">Idaho</option>
-                    <option value="IL">Illinois</option>
-                    <option value="IN">Indiana</option>
-                    <option value="IA">Iowa</option>
-                    <option value="KS">Kansas</option>
-                    <option value="KY">Kentucky</option>
-                    <option value="LA">Louisiana</option>
-                    <option value="ME">Maine</option>
-                    <option value="MD">Maryland</option>
-                    <option value="MA">Massachusetts</option>
-                    <option value="MI">Michigan</option>
-                    <option value="MN">Minnesota</option>
-                    <option value="MS">Mississippi</option>
-                    <option value="MO">Missouri</option>
-                    <option value="MT">Montana</option>
-                    <option value="NE">Nebraska</option>
-                    <option value="NV">Nevada</option>
-                    <option value="NH">New Hampshire</option>
-                    <option value="NJ">New Jersey</option>
-                    <option value="NM">New Mexico</option>
-                    <option value="NY">New York</option>
-                    <option value="NC">North Carolina</option>
-                    <option value="ND">North Dakota</option>
-                    <option value="OH">Ohio</option>
-                    <option value="OK">Oklahoma</option>
-                    <option value="OR">Oregon</option>
-                    <option value="PA">Pennsylvania</option>
-                    <option value="RI">Rhode Island</option>
-                    <option value="SC">South Carolina</option>
-                    <option value="SD">South Dakota</option>
-                    <option value="TN">Tennessee</option>
-                    <option value="TX">Texas</option>
-                    <option value="UT">Utah</option>
-                    <option value="VT">Vermont</option>
-                    <option value="VA">Virginia</option>
-                    <option value="WA">Washington</option>
-                    <option value="WV">West Virginia</option>
-                    <option value="WI">Wisconsin</option>
-                    <option value="WY">Wyoming</option>
-                </select>
+            <div class="form-group">
+                <label for="State">State</label>
+                <input type="text" name="state" class="form-control" id="State" placeholder="ie New Jersey">
             </div>
             <div class="form-group col-md-2">
-                <label for="inputZip">Zip</label>
-                <input type="text" name="zip" class="form-control" id="inputZip" required>
+                <label for="Zip">Zip</label>
+                <input type="text" name="zip" class="form-control" id="Zip" required>
             </div>
         </div>
         <h1> PAYMENT METHOD </h1>
